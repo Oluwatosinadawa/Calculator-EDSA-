05b3658b6c6a1b9a1b9564123418bf77f9a12bb3
Oluwatosin Adawa <oluwatosin.adawa@outlook.com>
2023-03-16 10:41:18 +0100

    second

--- calc.js
+++ calc.js
@@ -8,14 +8,15 @@ function clearResult() {
 function backspace() {
     document.getElementById('result').value = document.getElementById('result').value.slice(0,-1);
 }
-function percentage (){
-    document.getElementById('result').value = document.getElementById('result').replace(/%/g, "/100*");
-}
-const part = 10;
-const whole = 10;
-console.log(percentage);
-
 function calculate() {
     var result = document.getElementById('result').value;
-    document.getElementById('result').value = eval(result);
+    if (result.includes('%')) {
+        var parts = result.split('%');
+        var num = parseFloat(parts[0]);
+        var percent = parseFloat(parts[1]);
+        document.getElementById('result').value = num * (percent / 100);
+    }
+     else {
+        document.getElementById('result').value = eval(result);
+    }
 }
